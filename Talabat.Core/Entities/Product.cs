b770874e1324ad5ffdc82600ffbbccc806973c2a
ahmedf777686 <142512678+ahmedf777686@users.noreply.github.com>
2024-04-13 22:41:03 +0200

    Part 08 Product Module - Entities

---
+++ Talabat.Core/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+    public class Product :BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set;}
+
+        public int Brand { get; set; } //foreign key
+        public ProductBrand BroductBrand { get; set; } // Navigational Property
+
+
+
+        public int ProductCategoryId { get; set; } //foreign key
+        public ProductCategory BroductCategory { get; set; } // Navigational Property
+
+
+    }
+}
