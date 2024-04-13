32e94a36e3dd9d49e702fa0824d2b423e18f580a
ahmedf777686 <142512678+ahmedf777686@users.noreply.github.com>
2024-04-14 01:47:01 +0200

    Part 10 Update Database

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -5,16 +5,17 @@ namespace Talabat.APIs
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
 
+            #region Configure Services
             builder.Services.AddControllers();
-            
 
-        
+
+
 
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,16 +23,33 @@ namespace Talabat.APIs
             builder.Services.AddSwaggerGen();
 
 
-              // OnConfiguring
+            // OnConfiguring
             builder.Services.AddDbContext<StoreContext>(Options =>
             {
                 Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionStrings"));
             });
 
+            #endregion
 
             var app = builder.Build();
 
+            using var Scope = app.Services.CreateScope();
+            var services = Scope.ServiceProvider;
+            var _Dbcontext = services.GetRequiredService<StoreContext>();
+            var loggerfactory = services.GetRequiredService<ILoggerFactory>();
+            try
+            {
+                await _Dbcontext.Database.MigrateAsync();
+            }
+            catch (Exception ex)
+            {
+
+                var logger = loggerfactory.CreateLogger<Program>();
+                logger.LogError("an Error has been occured during apply the migration");
+            }
+
             // Configure the HTTP request pipeline.
+            #region Configure Kestrel Middlewares
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -43,7 +61,11 @@ namespace Talabat.APIs
             app.UseAuthorization();
 
 
-            app.MapControllers(); 
+            app.MapControllers();
+            #endregion
+
+
+
 
             app.Run();
         }
