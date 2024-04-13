97f3d0847832a00cde5a61c933d819ea04e243fb
ahmedf777686 <142512678+ahmedf777686@users.noreply.github.com>
2024-04-13 19:20:58 +0200

    Part 03 Postman

--- Talabat.APIs/Controllers/WeatherForecastController.cs
+++ Talabat.APIs/Controllers/WeatherForecastController.cs
@@ -16,14 +16,14 @@ namespace Talabat.APIs.Controllers
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
-        }
+        } 
 
-        [HttpGet(Name = "GetWeatherForecast")] 
+        [HttpGet(Name = "GetWeatherForecast")]  
         public IEnumerable<WeatherForecast> Get()
-        {
+        {  
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Now.AddDays(index),
+                Date = DateTime.Now.AddDays(index),   
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
